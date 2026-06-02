645909043aac5469211052ca5668c8a71807f146
Dan Marsden <dan@danmarsden.com>
2026-06-02 15:24:34 +1200

    Remove old dependence on local_aws - aws libs now in core.

--- classes/local/store/s3/client.php
+++ classes/local/store/s3/client.php
@@ -25,8 +25,6 @@
 
 namespace tool_objectbackup\local\store\s3;
 
-use local_aws\admin_settings_aws_region;
-
 /**
  * Custom client class for objectbackup.
  */
@@ -45,30 +43,12 @@ class client extends \tool_objectfs\local\store\s3\client {
         global $OUTPUT;
         $plugins = \core_component::get_plugin_list('local');
 
-        if (!array_key_exists('aws', $plugins)) {
-            $text  = $OUTPUT->notification(new \lang_string('settings:aws:installneeded', OBJECTFS_PLUGIN_NAME));
-            $settings->add(new \admin_setting_heading('tool_objectbackup/aws',
-                new \lang_string('settings:aws:header', 'tool_objectbackup'), $text));
-            return $settings;
-        }
-
-        $plugin = (object)['version' => null];
-        if (file_exists($plugins['aws'].'/version.php')) {
-            include($plugins['aws'].'/version.php');
-        }
-        if (empty($plugin->version) || $plugin->version < 2020051200) {
-            $text  = $OUTPUT->notification(new \lang_string('settings:aws:upgradeneeded', OBJECTFS_PLUGIN_NAME));
-            $settings->add(new \admin_setting_heading('tool_objectbackup/aws',
-                new \lang_string('settings:aws:header', 'tool_objectbackup'), $text));
-            return $settings;
-        }
-
         $settings->add(new \admin_setting_heading('tool_objectbackup/aws',
             new \lang_string('settings:aws:header', 'tool_objectbackup'), $this->define_client_check()));
 
         $settings->add(new \admin_setting_configcheckbox('tool_objectbackup/s3_usesdkcreds',
             new \lang_string('settings:aws:usesdkcreds', 'tool_objectbackup'),
-            $this->define_client_check_sdk($config), ''));
+            '',''));
 
         if (empty($config->s3_usesdkcreds)) {
             $settings->add(new \admin_setting_configtext('tool_objectbackup/s3_key',
@@ -84,7 +64,7 @@ class client extends \tool_objectfs\local\store\s3\client {
             new \lang_string('settings:aws:bucket', 'tool_objectfs'),
             new \lang_string('settings:aws:bucket_help', 'tool_objectfs'), ''));
 
-        $settings->add(new admin_settings_aws_region('tool_objectbackup/s3_region',
+        $settings->add(new \admin_setting_configtext('tool_objectbackup/s3_region',
             new \lang_string('settings:aws:region', 'tool_objectfs'),
             new \lang_string('settings:aws:region_help', 'tool_objectfs'), ''));
 
